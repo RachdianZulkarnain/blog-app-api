29869c27204f10d99c569cba50717461c101203c
RachdianZulkarnain <rachdianmuhammadadha@gmail.com>
2025-07-30 15:19:18 +0700

    blog detail

--- src/modules/blog/blog.router.ts
+++ src/modules/blog/blog.router.ts
@@ -12,6 +12,7 @@ export class BlogRouter {
 
   private initializedRoutes = () => {
     this.router.get("/", this.blogController.getBlogs);
+    this.router.get("/:slug", this.blogController.getBlogBySlug);
   };
 
   getRouter = () => {
